02489070249f880912d5d6396442556cab8e45c6
chcam <73630763+chcam@users.noreply.github.com>
2021-06-23 23:26:19 +1000

    Reformatting for Best Player

--- src/commands/first.js
+++ src/commands/first.js
@@ -2,12 +2,12 @@ const fetch = require("node-fetch");
 const querystring = require("querystring");
 
 module.exports = {
-  name: "map",
-  description: "Map!",
+  name: "first",
+  description: "First player!",
   async execute(message, args) {
     const query = querystring.stringify({ term: args.join(" ") });
-    await fetch(`https://kagstats.com/api/maps`)
+    await fetch(`https://kagstats.com/api/leaderboard`)
       .then((response) => response.json())
-      .then((res) => message.channel.send(JSON.stringify(res[0])));
+      .then((res) => message.channel.send(JSON.stringify(res.leaderboard[0])));
   },
 };
