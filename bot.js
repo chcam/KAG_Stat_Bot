5a88bbe7cc7b12106dac8150ab2af205e79e4eee
chcam <73630763+chcam@users.noreply.github.com>
2021-06-23 16:53:26 +1000

    Setting up bot

--- bot.js
+++ bot.js
@@ -3,3 +3,8 @@ const config = require("./config.json");
 
 // creating bot
 const bot = new eris.Client(config.token);
+
+// When the bot is connected and ready, log to console.
+bot.on("ready", () => {
+  console.log("Connected and ready.");
+});
